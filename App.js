25881303bbd754668452fa0daa31225a6b1f008c
Serhiy Tymoshenko <sergtimosh@gmail.com>
2023-01-11 18:46:13 +0200

    add button

--- App.js
+++ App.js
@@ -4,7 +4,8 @@
  */
 
 import React, { useState } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View, Button } from 'react-native';
+import {plusIcon} from './icon-plus';
 import * as Screens from './Screens/index';
 
 export default function App() {
@@ -48,6 +49,18 @@ export default function App() {
           Welcome to the Detox Template. Please add a screen with your example/issue below.
         </Text>
       {renderScreenButton('Example screen', Screens.ExampleScreen)}
+      <Button
+          title='Press Me'
+          testID={'plus-icon'}
+          height={500}
+          width={500}
+          // shadowStyle={{...buttonShadow, radius: 5, border: scale(25), y: scale(10)}}
+          colors={'rgb(0,173,238)'}
+          style={{borderRadius: 5}}>
+          <View>
+            {/* <SVG svg={plusIcon} height={500} width={500} /> */}
+          </View>
+        </Button>
     </View>
   );
 }
