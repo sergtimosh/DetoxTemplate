6609933e9e1867f9a865b28e334919a2fdb763dc
Serhiy Tymoshenko <sergtimosh@gmail.com>
2023-01-11 18:46:44 +0200

    minors

--- e2e/ExampleScreen.e2e.js
+++ e2e/ExampleScreen.e2e.js
@@ -4,7 +4,10 @@ describe('Example Screen Tests', () => {
   });
 
   it('should type into the text field', async () => {
+    const timeout = async (t) => await new Promise(resolve => setTimeout(resolve, t))
+    await timeout(2000)
     await element(by.text('Example screen')).tap();
+    await timeout(2000)
 
     const someText = 'This is just a test (:';
     await element(by.id('ExampleTextInputId')).typeText(someText);
